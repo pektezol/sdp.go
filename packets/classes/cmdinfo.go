39ecba8a16a1ef734edb216f1fafb094c5c38745
Arda Serdar Pektezol <1669855+pektezol@users.noreply.github.com>
2022-11-07 17:55:50 +0300

    done until the hard part

---
+++ packets/classes/cmdinfo.go
@@ -0,0 +1,51 @@
+package classes
+
+import "github.com/pektezol/bitreader"
+
+func ParseCmdInfo(reader *bitreader.ReaderType, MSSC int) []CmdInfo {
+	var out []CmdInfo
+	for i := 0; i < MSSC; i++ {
+		flags := reader.TryReadInt32()
+		viewOrigin := []float32{
+			reader.TryReadFloat32(),
+			reader.TryReadFloat32(),
+			reader.TryReadFloat32(),
+		}
+		viewAngles := []float32{
+			reader.TryReadFloat32(),
+			reader.TryReadFloat32(),
+			reader.TryReadFloat32(),
+		}
+		localViewAngles := []float32{
+			reader.TryReadFloat32(),
+			reader.TryReadFloat32(),
+			reader.TryReadFloat32(),
+		}
+		viewOrigin2 := []float32{
+			reader.TryReadFloat32(),
+			reader.TryReadFloat32(),
+			reader.TryReadFloat32(),
+		}
+		viewAngles2 := []float32{
+			reader.TryReadFloat32(),
+			reader.TryReadFloat32(),
+			reader.TryReadFloat32(),
+		}
+		localViewAngles2 := []float32{
+			reader.TryReadFloat32(),
+			reader.TryReadFloat32(),
+			reader.TryReadFloat32(),
+		}
+		cmdInfo := CmdInfo{
+			Flags:            int32(flags),
+			ViewOrigin:       viewOrigin,
+			ViewAngles:       viewAngles,
+			LocalViewAngles:  localViewAngles,
+			ViewOrigin2:      viewOrigin2,
+			ViewAngles2:      viewAngles2,
+			LocalViewAngles2: localViewAngles2,
+		}
+		out = append(out, cmdInfo)
+	}
+	return out
+}
