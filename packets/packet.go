0099caba88312ebdacef7c5db5b506c6c308a8a8
Arda Serdar Pektezol <1669855+pektezol@users.noreply.github.com>
2022-11-10 23:39:55 +0300

    remove hexadecimal case statements

--- packets/packet.go
+++ packets/packet.go
@@ -15,7 +15,7 @@ func ParsePacket(reader *bitreader.ReaderType) (status int) {
 	messageTick := reader.TryReadInt32()
 	messageSlot := reader.TryReadInt8()
 	switch messageType {
-	case 0x01: // TODO: SignOn - Data
+	case 1: // TODO: SignOn - Data
 		signOn := SignOn{
 			PacketInfo:  classes.ParseCmdInfo(reader, MSSC),
 			InSequence:  int32(reader.TryReadInt32()),
@@ -25,7 +25,7 @@ func ParsePacket(reader *bitreader.ReaderType) (status int) {
 		reader.SkipBytes(int(signOn.Size))
 		fmt.Printf("[%d] (%d) {%d} SignOn: %v\n", messageTick, messageType, messageSlot, signOn)
 		return 1
-	case 0x02: // TODO: Packet - Data
+	case 2: // TODO: Packet - Data
 		packet := Packet{
 			PacketInfo:  classes.ParseCmdInfo(reader, MSSC),
 			InSequence:  int32(reader.TryReadInt32()),
@@ -35,18 +35,18 @@ func ParsePacket(reader *bitreader.ReaderType) (status int) {
 		packet.Data = messages.ParseMessage(reader.TryReadBytesToSlice(int(packet.Size)))
 		fmt.Printf("[%d] (%d) Packet: %v\n", messageTick, messageType, packet)
 		return 2
-	case 0x03:
+	case 3:
 		syncTick := SyncTick{}
 		fmt.Printf("[%d] (%d) SyncTick: %v\n", messageTick, messageType, syncTick)
 		return 3
-	case 0x04:
+	case 4:
 		consoleCmd := ConsoleCmd{
 			Size: int32(reader.TryReadInt32()),
 		}
 		consoleCmd.Data = reader.TryReadStringLen(int(consoleCmd.Size))
-		fmt.Printf("[%d] (%d) ConsoleCmd: %s\n", messageTick, messageType, consoleCmd.Data)
+		// fmt.Printf("[%d] (%d) ConsoleCmd: %s\n", messageTick, messageType, consoleCmd.Data)
 		return 4
-	case 0x05: // TODO: UserCmd - Buttons
+	case 5: // TODO: UserCmd - Buttons
 		userCmd := UserCmd{
 			Cmd:  int32(reader.TryReadInt32()),
 			Size: int32(reader.TryReadInt32()),
@@ -54,24 +54,24 @@ func ParsePacket(reader *bitreader.ReaderType) (status int) {
 		userCmd.Data = classes.ParseUserCmdInfo(reader.TryReadBytesToSlice(int(userCmd.Size)))
 		// fmt.Printf("[%d] (%d) UserCmd: %v\n", messageTick, messageType, userCmd)
 		return 5
-	case 0x06: // TODO: DataTables
+	case 6: // TODO: DataTables
 		val := reader.TryReadInt32()
 		reader.SkipBytes(int(val))
 		// fmt.Printf("[%d] (%d) DataTables: \n", messageTick, messageType)
 		return 6
-	case 0x07:
+	case 7:
 		stop := Stop{
 			RemainingData: nil,
 		}
 		fmt.Printf("[%d] (%d) Stop: %v\n", messageTick, messageType, stop)
 		return 7
-	case 0x08: // TODO: CustomData
+	case 8: // TODO: CustomData
 		reader.SkipBytes(4)
 		val := reader.TryReadInt32()
 		reader.SkipBytes(int(val))
 		// fmt.Printf("[%d] (%d) CustomData: \n", messageTick, messageType)
 		return 8
-	case 0x09: // TODO: StringTables - Data
+	case 9: // TODO: StringTables - Data
 		stringTables := StringTables{
 			Size: int32(reader.TryReadInt32()),
 		}
